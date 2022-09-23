195968f14300857c686b5c6a8e89656f36aaa915
VelizarDabov <velizardabov@gmail.com>
2022-09-23 08:07:50 +0300

    add forkify app
    add a compleate forkify project

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '57c759c3-e5dc-4d18-90ad-bef60eebcc69';
+export const MODAL_CLOSE_SEC = 2.5;
